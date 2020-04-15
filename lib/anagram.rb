e44659e97cd669022fd16f3d5bab3e1a0474e9a5
Joseph Kao <josephtkao@gmail.com>
2020-04-15 16:51:56 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,7 +1,18 @@
 # Your code goes here!
 class Anagram
+
+  attr_accessor :word, :array
   def initialize(array)
-    @array = array
+    @word = array
   end
+
+  def match(array)
+    letters = @word.split.sort
+    match = []
+    array.each do |word|
+      if word.split.sort == letters
+        match << word
+      end
+    end
 
 end
